9459b1af9df0b01e21c761a709a67cd51365a45a
SmutnyJan <smutnyjan2003@seznam.cz>
2025-04-02 01:39:50 +0200

    Update FavoriteTickersController.cs

--- Eagle_web_api/Eagle_web_api/Controllers/FavoriteTickersController.cs
+++ Eagle_web_api/Eagle_web_api/Controllers/FavoriteTickersController.cs
@@ -66,7 +66,7 @@ namespace Eagle_web_api.Controllers
 
 
         [HttpPost]
-        public async Task<ActionResult<FavoriteTicker>> PostFavoriteTicker(string favoriteTicker)
+        public async Task<ActionResult<FavoriteTicker>> PostFavoriteTicker([FromBody] string favoriteTicker)
         {
             if (string.IsNullOrWhiteSpace(favoriteTicker))
             {
